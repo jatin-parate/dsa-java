f3c098c24cb1c507c5c169bee1b57b07a80bc0c6
jatin <jatin.parate@mlveda.com>
2024-08-25 14:37:00 +0530

    Found_String_Reverse String

---
+++ src/com/hackerrank/string/reverseString/Solution.java
@@ -0,0 +1,25 @@
+package com.hackerrank.string.reverseString;
+
+import java.util.Scanner;
+
+public class Solution {
+
+	private static String reverseString(String line) {
+		char[] outputArr = new char[line.length()];
+
+		for (int i = line.length() - 1; i >= 0; i--) {
+			outputArr[line.length() - i - 1] = line.charAt(i);
+		}
+
+		return new StringBuilder(line.length()).append(outputArr).toString();
+	}
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		String line = sc.nextLine();
+
+		System.out.println(reverseString(line));
+		sc.close();
+	}
+
+}
